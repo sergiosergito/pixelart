ba1145aa012b571f479d8ae1d69777da622504be
serginho6x <serginho_6x@hotmail.com>
2020-07-15 17:11:48 -0400

    javascript generates grid

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-
-
-/*
-var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-
-var i;
-for (i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
-}*/
-
 var sizeOfCanvas = 4;
 var text = "";
-for (var row = 0; row < 4; row++) {
+for (var row = 0; row < sizeOfCanvas; row++) {
 	text+="<div class='row'>";
 	//text="";
 	for (var cell = 0; cell < sizeOfCanvas; cell++){
@@ -18,4 +8,4 @@ for (var row = 0; row < 4; row++) {
 	}
 	text+="</div>";
 }
-document.getElementById("demo").innerHTML = text;
+document.getElementById("generateGrid").innerHTML = text;
