70451e5178ddea9b0df15f3d82e6ab5d6a1e072b
serginho6x <serginho_6x@hotmail.com>
2020-07-16 10:51:38 -0400

    cells have ID

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 function generateGrid(){
 	var sizeOfCanvas = 4;
+	var cellID=1;
 	var text = "";
 	for (var row = 0; row < sizeOfCanvas; row++) {
 		text+="<div class='row'>";
 		for (var cell = 0; cell < sizeOfCanvas; cell++){
-			text += "<div class='pixel'></div>"
+			text += "<div class='pixel' id= " + cellID + " ></div>";
+			cellID++;
 		}
 		text+="</div>";
 	}
